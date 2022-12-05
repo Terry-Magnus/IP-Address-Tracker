df347c3a113b8c1bcacfb904331828a1199e25cd
Michael Uyanna <crackmastermike@gmail.com>
2022-12-05 14:02:52 +0100

    updated location marker icon

--- src/components/location-bar.js
+++ src/components/location-bar.js
@@ -3,7 +3,7 @@ import React from "react";
 const LocationBar = ({ info }) => {
     return (
         <div className="location-bar">
-            <div className="">IP ADDRESS<br /><span>{`${info.ip}`}</span></div>
+            <div>IP ADDRESS<br /><span>{`${info.ip}`}</span></div>
             <div>LOCATION<br />
                 <span>{`${info.location.city}, ${info.location.country} ${info.location.postalCode}`}</span>
             </div>
